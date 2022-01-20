119a4d11e91089321c54de686fa6783c5f1d2ccf
RaquelMRJ <97487302+RaquelMRJ@users.noreply.github.com>
2022-01-20 14:06:27 -0600

    Add files via upload

---
+++ custom_modules/assets/dbpostgres.products.sql
@@ -0,0 +1,24 @@
+-- Table: dbpostgres.products
+
+--DROP TABLE dbpostgres.products;
+CREATE SCHEMA dbpostgres;
+
+CREATE TABLE IF NOT EXISTS dbpostgres.products
+(
+    id SERIAL PRIMARY KEY,
+    producto VARCHAR(255),
+    presentacion VARCHAR(255),
+    marca VARCHAR(255),
+    categoria VARCHAR(255),
+    catalogo VARCHAR(255),
+    precio FLOAT,
+    fecharegistro DATE,
+    cadenacomercial VARCHAR(255),
+    giro VARCHAR(255),
+    nombrecomercial VARCHAR(255),
+    direccion VARCHAR(255),
+    estado VARCHAR(255),
+    municipio VARCHAR(255),
+    latitud FLOAT,
+    longitud FLOAT    
+);
